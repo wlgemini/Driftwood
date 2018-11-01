d3d1ac7bcfea46225e0c5b6ea2e168997e25f70d
王路广 <wangluguang@finupgroup.com>
2018-11-01 15:33:59 +0800

    add demo layout

--- Example/Driftwood/UIView+Driftwood.swift
+++ Example/Driftwood/UIView+Driftwood.swift
@@ -15,7 +15,7 @@ extension UIView {
         let v = T()
         v.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(v)
-        let c = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        let c = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 0.5)
         v.backgroundColor = c
         return v
     }
--- Example/Driftwood/ViewController.swift
+++ Example/Driftwood/ViewController.swift
@@ -11,33 +11,49 @@ import Driftwood
 
 class ViewController: UIViewController {
 
-    private weak var v1: UIView!
-    private weak var v2: UIView!
-    private weak var v3: UIView!
-    private weak var v4: UIView!
-    private weak var v5: UIView!
+    private weak var lb1: UILabel!
+    private weak var lb2: UILabel!
+    private weak var lb3: UILabel!
+    private weak var lb4: UILabel!
+    private weak var lb5: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.v1 = self.view.createSubView()
-        self.v1.dw.make.left(0).right(0).top(0).bottom(0, to: self.view.safeAreaLayoutGuide.dw.top)
+        self.lb1 = self.view.createSubView()
+        self.lb1.text = "1"
+        self.lb1.textAlignment = .center
+        self.lb1.dw.make.left(0).right(0).top(0, to: self.view.safeAreaLayoutGuide.dw.top).height(50)
         
-        self.v2 = self.view.createSubView()
-        self.v2.dw.make.left(0).right(0).top(0, to: self.view.safeAreaLayoutGuide.dw.bottom).bottom(0)
+        self.lb2 = self.view.createSubView()
+        self.lb2.text = "2"
+        self.lb2.textAlignment = .center
+        self.lb2.dw.make.left(0).right(0).bottom(0, to: self.view.safeAreaLayoutGuide.dw.bottom).height(50)
         
-        self.v3 = self.view.createSubView()
-        self.v3.dw.make.centerX(0).centerY(0).width(200).height(300)
+        self.lb3 = self.view.createSubView()
+        self.lb3.text = "3"
+        self.lb3.textAlignment = .center
+        self.lb3.dw.make.centerXY(offsets: .zero).width(250).height(300)
 
+        self.lb4 = self.view.createSubView()
+        self.lb4.text = "4"
+        self.lb4.textAlignment = .center
+        self.lb4.dw.make.centerXY(offsets: CGPoint(x: 50, y: 50), to: self.lb3).size(to: self.lb3)
         
-        self.v4 = self.view.createSubView()
-        self.v4.dw.make.centerXY(offsets: CGPoint(x: 50, y: 50)).size(to: self.v3)
-        
-        self.v5 = self.view.createSubView()
-        self.v5.dw.make.edge(insets: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20), to: self.v4)
+        self.lb5 = self.view.createSubView()
+        self.lb5.text = "5"
+        self.lb5.textAlignment = .center
+        self.lb5.dw.make.edge(insets: UIEdgeInsets(top: 0, left: 0, bottom: 30, right: 30), to: self.lb4)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        self.lb3.dw.update.centerXY(offsets: CGPoint(x: -50, y: -50))
+        
+        self.lb4.dw.update.centerXY(offsets: CGPoint(x: 150, y: 150))
+        self.lb4.dw.remove.size()
+        self.lb4.dw.make.size(to: self.lb3, multiply: 0.75)
+        
+        self.lb5.dw.update.edge(insets: UIEdgeInsets(top: 30, left: 30, bottom: 0, right: 0))
     }
 }
